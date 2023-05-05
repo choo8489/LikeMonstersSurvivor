c6d5fe39dc5cb6fd257d5ac15f17ebcb0ab6cb60
choo8489 <choo9959@gmail.com>
2023-05-05 21:47:34 +0900

    Create enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D target;
+
+    private bool isLive;
+
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        TryGetComponent(out rigidbody);
+        TryGetComponent(out spriteRenderer);
+        TryGetComponent(out animator);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 directionVector = target.position - rigidbody.position;
+        Vector2 nextVector = directionVector.normalized * speed * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(rigidbody.position + nextVector);
+        rigidbody.velocity = Vector2.zero;
+    }
+}
